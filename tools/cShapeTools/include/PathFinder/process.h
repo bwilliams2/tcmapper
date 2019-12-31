fb355e40988996ec6f37a8dafcbb5245417cff4f
Bryce Williams <bwilliams2@gmail.com>
2019-12-30 20:03:53 -0600

    New project structure

---
+++ tools/cShapeTools/include/PathFinder/process.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#ifndef _PROCESS_H_
+#define _PROCESS_H_
+
+    void makeArray(double longlat[][2], int numrows, FILE *ifp);
+    void geoConversion(double longlat_array[][2], int line_nums);
+    void distanceCalc(double longlat_array[][2], double distance[], int line_nums, PJ_COORD base);
+    void geoSearch(DBFHandle dbfH, double baseLong, double baseLat, int searchRadius, double * distances);
+
+#endif
