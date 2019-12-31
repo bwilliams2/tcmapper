6f5745aaf7cff3631e892116c2018987375b96ff
Bryce Williams <bwilliams2@gmail.com>
2019-12-30 21:03:27 -0600

    update webapp

--- Application/cShapeTools/include/PathFinder/process.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#ifndef _PROCESS_H_
-#define _PROCESS_H_
-
-    void makeArray(double longlat[][2], int numrows, FILE *ifp);
-    void geoConversion(double longlat_array[][2], int line_nums);
-    void distanceCalc(double longlat_array[][2], double distance[], int line_nums, PJ_COORD base);
-
-#endif
