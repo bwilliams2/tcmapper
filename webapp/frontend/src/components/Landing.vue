7c1286401ac61dbbf0c1e4aa84d188405edc272c
Bryce Williams <bwilliams2@gmail.com>
2020-04-30 07:23:02 -0500

    Page updates

--- webapp/frontend/src/components/Landing.vue
+++ webapp/frontend/src/components/Landing.vue
@@ -1,6 +1,18 @@
 <template>
-  <v-container>
-    <v-row class="text-center" justify="center">
+  <v-container fluid>
+    <v-col cols="12">
+      <v-row
+        align="center"
+        justify="center"
+        class="grey lighten-5"
+        style="height: 300px;"
+      >
+        <v-card v-for="n in 3" :key="n" class="ma-3 pa-6" outlined tile>
+          Column
+        </v-card>
+      </v-row>
+    </v-col>
+    <!-- <v-row justify="end">
       <v-col cols="6">
         <v-card-text>
           Enter base address
@@ -9,35 +21,15 @@
           <v-autocomplete
           :value="address"
           @input="updateAddress"
-            :items="items"
-            :loading="isLoading"
-            :search-input.sync="search"
-            color="white"
-            hide-no-data
-            hide-selected
-            item-text="Description"
-            item-value="API"
-            label="Public APIs"
-            placeholder="Start typing to Search"
-            prepend-icon="mdi-database-search"
-            return-object
+          :items="items"
+          :loading="isLoading"
+          :search-input.sync="search"
+          filled
           ></v-autocomplete>
         </v-card-text>
-        <v-divider></v-divider>
-        <v-text-field
-          placeholder="Address"
-          solo
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="text-center" justify="center">
-      <v-col cols="3">
         <v-btn>Find My House</v-btn>
       </v-col>
-      <v-col cols="3">
-      </v-col>
-
-    </v-row>
+    </v-row> -->
   </v-container>
 </template>
 
@@ -51,32 +43,32 @@ export default Vue.extend({
     isLoading: false,
     search: null,
     entries: [],
-    descriptionLimit: 60,
+    descriptionLimit: 60
   }),
   computed: {
-    items () {
-      return ["6643 Pelican Pl", "600 Blake St"]
+    items() {
+      return ["6643 Pelican Pl", "600 Blake St"];
     },
     ...mapState(["address"])
   },
   methods: {
-    updateAddress (e: Event) {
+    updateAddress(e: Event) {
       if (event?.target) {
-        const target= event.target as HTMLInputElement;
+        const target = event.target as HTMLInputElement;
         this.$store.commit("updateAddress", target.value);
       }
     }
   },
   watch: {
-    search (val) {
+    search(val) {
       // Items have already been loaded
-      if (this.items.length > 0) return
+      if (this.items.length > 0) return;
 
       // Items have already been requested
-      if (this.isLoading) return
+      if (this.isLoading) return;
 
-      this.isLoading = true
-      
+      this.isLoading = true;
+
       // Lazily load input items
       // fetch('https://api.publicapis.org/entries')
       //   .then(res => res.json())
