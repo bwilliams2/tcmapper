46a1d5cebde2937d5f8ca2a1454368b4e6f2cbb3
Bryce Williams <bwilliams2@gmail.com>
2020-05-03 20:21:18 -0500

    Switch to HERE

--- webapp/frontend/src/components/Landing.vue
+++ webapp/frontend/src/components/Landing.vue
@@ -24,19 +24,61 @@
 <script lang="ts">
 import Vue from "vue";
 import { mapState } from "vuex";
-const apiKey = process.env.PLACES_API_KEY;
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import hereInit from "../utils/here";
+const apiKey = process.env.HERE_API_KEY as string;
+
+export interface HereAddress {
+  title: string;
+  id: string;
+  resultType: string;
+  houseNumberType: string;
+  address: {
+    label: string;
+    countryCode: string;
+    countryName: string;
+    state: string;
+    county: string;
+    city: string;
+    street: string;
+    postalCode: string;
+    houseNumber: string;
+  };
+  position: {
+    lat: number;
+    lng: number;
+  };
+  access: [
+    {
+      lat: number;
+      lng: number;
+    }
+  ];
+  distance: number;
+}
+
+interface LandingData {
+  isLoading: boolean;
+  search: string | null;
+  entries: HereAddress[];
+  descriptionLimit: number;
+}
 
 export default Vue.extend({
   name: "Landing",
-  data: () => ({
+  data: (): LandingData => ({
     isLoading: false,
     search: null,
     entries: [],
     descriptionLimit: 60
   }),
+  // mounted() {
+  //   this.sessionToken = uuidv4();
+  // },
   computed: {
-    items() {
-      return ["6643 Pelican Pl", "600 Blake St"];
+    items(): string[] {
+      return this.entries.map(e => e.title);
     },
     ...mapState(["address"])
   },
@@ -49,7 +91,9 @@ export default Vue.extend({
     }
   },
   watch: {
-    search(val) {
+    async search(val) {
+      const H = (await hereInit()) as any;
+
       // Items have already been loaded
       if (this.items.length > 0) return;
 
@@ -58,19 +102,47 @@ export default Vue.extend({
 
       this.isLoading = true;
 
+      // Instantiate a map and platform object:
+      const platform = new H.service.Platform({
+        apikey: apiKey
+      });
+      // Get an instance of the search service:
+      const service = platform.getSearchService();
+
+      // Call the "autosuggest" method with the search parameters,
+      // the callback and an error callback function (called if a
+      // communication error occurs):
+      service.autosuggest(
+        {
+          // Search query
+          q: val,
+          // Center of the search context
+          in: "circle:44.985207,-93.264622;r=50000"
+        },
+        (result: any) => {
+          const { position, title } = result.items[0];
+          // Assumption: ui is instantiated
+          // Create an InfoBubble at the returned location
+          // ui.addBubble(new H.ui.InfoBubble(position, {
+          //   content: title
+          // }));
+        },
+        alert
+      );
+
       // Lazily load input items
-      // fetch('https://api.publicapis.org/entries')
-      //   .then(res => res.json())
+      // axios.get(
+      //   'https://autosuggest.search.hereapi.com/v1/autosuggest',
+      //   { params: { q: val, in: "circle:44.985207,-93.264622;r=50000", apiKey: apiKey } },
+      // )
       //   .then(res => {
-      //     const { count, entries } = res
-      //     this.count = count
-      //     this.entries = entries
-      //   })
-      //   .catch(err => {
-      //     console.log(err)
-      //   })
-      //   .finally(() => (this.isLoading = false))
-      // },
+      //     const d =0;
+      //     this.entries = res.data;
+      //  })
+      // .catch(err => {
+      //   console.log(err)
+      // })
+      // .finally(() => (this.isLoading = false))
     }
   }
 });
