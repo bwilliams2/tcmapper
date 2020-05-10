8727bd338c6f0bf7742b0192118252f77dd5d20a
Bryce Williams <bwilliams2@gmail.com>
2020-05-09 21:51:19 -0500

    Update geosearch Cython interface

--- webapp/backend/backend/geotools/distance_calc.pyx
+++ webapp/backend/backend/geotools/distance_calc.pyx
@@ -1,12 +1,11 @@
 import numpy as np
 
-cdef extern from "../../../../tools/cShapeTools/include/PathFinder/process.h":
-
+cdef extern from "/home/bryce/code/commute/tools/cShapeTools/include/PathFinder/process.c":
     void geoSearch(char * fname, double baseLong, double baseLat, int searchRadius, double * distances)
 
-def getDistances(bytes fname, double baseLong, double baseLat, int searchRadius, int recordLength):
+cpdef get_distances(bytes fname, double baseLong, double baseLat, int searchRadius, int recordLength):
     distances_array = np.empty((recordLength * 2), np.doublec)
     cdef double[:] distances = distances_array
     cdef char * cfname = fname
     geoSearch(cfname, baseLong, baseLat, searchRadius, &distances[0])
-    return distances_array
+    return distances_array.reshape(1, 2)
