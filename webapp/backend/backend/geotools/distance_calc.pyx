f3f2e0787e3691a463dd83338f963ffa8ff0ba82
Bryce Williams <bwilliams2@gmail.com>
2020-05-10 09:46:47 -0500

    Working distance search

--- webapp/backend/backend/geotools/distance_calc.pyx
+++ webapp/backend/backend/geotools/distance_calc.pyx
@@ -1,11 +1,13 @@
 import numpy as np
+cimport numpy as np
+
 
 cdef extern from "/home/bryce/code/commute/tools/cShapeTools/include/PathFinder/process.c":
     void geoSearch(char * fname, double baseLong, double baseLat, int searchRadius, double * distances)
 
 cpdef get_distances(bytes fname, double baseLong, double baseLat, int searchRadius, int recordLength):
-    distances_array = np.empty((recordLength * 2), np.doublec)
-    cdef double[:] distances = distances_array
+    distances_array = np.ascontiguousarray(np.zeros((recordLength, dtype=np.double) - 1.)
+    cdef double[::1] distances = distances_array
     cdef char * cfname = fname
     geoSearch(cfname, baseLong, baseLat, searchRadius, &distances[0])
-    return distances_array.reshape(1, 2)
+    return distances_array
--- webapp/backend/backend/geotools/path_calc.py
+++ webapp/backend/backend/geotools/path_calc.py
@@ -1,19 +1,35 @@
 from pathlib import Path
 import glob
+from importlib.resources import path, contents
 
 import fiona
+import numpy as np
+import pandas as pd
 
 from .distance_calc import get_distances
 
 def address_search(longitude: float, latitude: float, distance: float):
-    files = [bytes(Path(file).absolute()) for file in glob.glob("../../../data/*.shp")]
+    with path("backend", "data") as data_dir:
+        files = [bytes(Path(file).absolute()) for file in glob.glob(str(data_dir.joinpath("*.shp").absolute()))]
+    all_distances = np.array([]) 
     county_addresses = []
     for file in files:
-        data = fiona.open(str(file))
-        record_length = len(data)
-        distances = get_distances(file, longitude, latitude, distance, record_length)
-        record_positions = distances[:, 0]
-        file_records = [data[i]["properties"] for i in record_positions]
+        with fiona.open(file.decode()) as data:
+            record_length = len(data)
+        distances = get_distances(file, latitude, longitude, distance, record_length)
+        record_positions = np.where(distances <= distance)
+        with fiona.open(file.decode()) as data:
+            file_records = []
+            for location in record_positions[0].tolist():
+                try:
+                    file_records.append(data[location]["properties"])
+                except:
+                    print(type(location))
+                    print(location)
+                    print(file)
         county_addresses += file_records
-    return county_addresses
+        all_distances = np.concatenate([all_distances, distances[record_positions[0]]])
+    data = pd.DataFrame(county_addresses)
+    data["DISTANCE"] = all_distances
+    return data
 
