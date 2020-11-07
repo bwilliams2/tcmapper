c05db0466e0e6b62c40bf71c635d3dcd0365928a
Bryce Williams <bwilliams2@gmail.com>
2020-11-07 07:21:00 -0600

    Data analysis updates

--- webapp/frontend/src/main.ts
+++ webapp/frontend/src/main.ts
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import { createApp } from "vue";
 import App from "./components/App.vue";
 import router from "./router";
 import store from "./store";
@@ -11,5 +11,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
