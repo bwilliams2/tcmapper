97554f67b6400a85adea968f8a05d6821bc25c2e
Bryce Williams <bwilliams2@gmail.com>
2020-11-09 22:40:40 -0600

    Update map handling

--- webapp/frontend/src/components/NewMap.vue
+++ webapp/frontend/src/components/NewMap.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="map" style="width: 100%; height: 100%;"></div>
+  <div id="map" style="width: 100%; height: 100%"></div>
 </template>
 
 <script lang="ts">
@@ -10,6 +10,7 @@ import { mapState } from "vuex";
 import * as d3 from "d3";
 import { GeoProjection } from "d3-geo";
 import { FeatureCollection } from "geojson";
+import "leaflet.heat";
 const apiKey = process.env.VUE_APP_HERE_API_KEY as string;
 
 interface Feature {
@@ -28,7 +29,7 @@ interface Features {
 
 interface MapData {
   url: string;
-  zoom: number;
+  zoom: { start: number; end: number };
   center: LatLngTuple;
   bounds: LatLngBounds | null;
   map: any;
@@ -77,7 +78,7 @@ export default Vue.extend({
   data(): MapData {
     return {
       url: `https://2.base.maps.ls.hereapi.com/maptile/2.1/maptile/newest/normal.day/{z}/{x}/{y}/512/png8?apiKey=${apiKey}&ppi=320`,
-      zoom: 12,
+      zoom: { start: 12, end: 12 },
       center: [47.41322, -1.219482],
       bounds: null,
       map: null,
@@ -87,7 +88,7 @@ export default Vue.extend({
       tileLayer: null,
       circle: {
         center: [47.41322, -1.0482],
-        radius: 4500,
+        radius: 14,
         color: "red",
       },
       features: {
@@ -103,7 +104,7 @@ export default Vue.extend({
     initMap() {
       this.map = L.map("map", {
         center: this.center,
-        zoom: this.zoom,
+        zoom: this.zoom.start,
       });
       this.tileLayer = L.tileLayer(this.url, {
         maxZoom: 18,
@@ -114,6 +115,8 @@ export default Vue.extend({
           '<a href="http://developer.here.com">HERE</a>',
       });
       this.tileLayer.addTo(this.map);
+      // @ts-ignore
+      // L.heatLayer(points).addTo(this.map)
       const parentDiv = document.getElementById("map");
       if (parentDiv !== null) {
         this.svg = d3
@@ -143,6 +146,7 @@ export default Vue.extend({
       //   .append("path")
       // feature.attr("d", path);
       const circleCoords = this.circle.center;
+      // let circleRadius = this.circle.radius;
       const feature = this.svgGroup
         .selectAll("circle")
         .data([circleCoords])
@@ -154,7 +158,7 @@ export default Vue.extend({
         .attr("cy", function (d: DataPoints) {
           return map.latLngToLayerPoint(d).y;
         })
-        .attr("r", 14)
+        .attr("r", this.circle.radius)
         .style("fill", "red")
         .attr("stroke", "red")
         .attr("stroke-width", 3)
@@ -163,6 +167,11 @@ export default Vue.extend({
       const svg = this.svg;
       const svgGroup = this.svgGroup;
       const self = this;
+
+      const currentZoom = {
+        start: map.getZoom(),
+        end: map.getZoom(),
+      };
       function update() {
         self.svgGroup
           .selectAll("circle")
@@ -173,23 +182,66 @@ export default Vue.extend({
             return map.latLngToLayerPoint(d).y;
           });
         const bounds = path.bounds(collection);
-        const topLeft = bounds[0];
-        const bottomRight = bounds[1];
-        svg
-          .attr("height", bottomRight[0] - topLeft[0])
-          .attr("width", bottomRight[1] - topLeft[1])
-          .style("left", topLeft[0] + "px")
-          .style("top", topLeft[1] + "px");
-        svgGroup.attr(
-          "transform",
-          "translate(" + -topLeft[0] + "," + -topLeft[1] + ")"
-        );
+        // let topLeft = bounds[0];
+        // let bottomRight = bounds[1];
+        currentZoom.end = map.getZoom();
+        const diff = currentZoom.start - currentZoom.end;
+        if (diff > 0) {
+          self.circle.radius = self.circle.radius / 2;
+        } else if (diff < 0) {
+          self.circle.radius = self.circle.radius * 2;
+        }
+        self.drawCircles([circleCoords]);
+        // If there is only one circle the bounds are defined by a single point
+        // Need to adjust for that here
+        // if (topLeft[0] === bottomRight[0]) {
+        //   topLeft = [topLeft[0] - 2 * circleRadius, topLeft[1] - 2 * circleRadius];
+        //   bottomRight = [bottomRight[0] + 2 * circleRadius, bottomRight[1] + 2 * circleRadius];
+        // }
+        // svg
+        //   .attr("height", bottomRight[0] - topLeft[0])
+        //   .attr("width", bottomRight[1] - topLeft[1])
+        //   .style("left", topLeft[0] + "px")
+        //   .style("top", topLeft[1] + "px");
+        // svgGroup.attr(
+        //   "transform",
+        //   "translate(" + -topLeft[0] + "," + -topLeft[1] + ")"
+        // );
       }
+      map.on("zoomstart", function () {
+        currentZoom.start = map.getZoom();
+      });
+      // if (diff > 0) { circle.setRadius(circle.getRadius() * 2); } else if (diff < 0) { circle.setRadius(circle.getRadius() / 2); }
       map.on("viewreset", update);
+      map.on("moveend", update);
       update();
     },
+    drawCircles(circleCoords: [number, number][]) {
+      const map = this.map;
+      const self = this;
+      this.svgGroup
+        .selectAll("circle")
+        .data(circleCoords)
+        .enter()
+        .append("circle")
+        .attr("cx", function (d: DataPoints) {
+          return map.latLngToLayerPoint(d).x;
+        })
+        .attr("cy", function (d: DataPoints) {
+          return map.latLngToLayerPoint(d).y;
+        })
+        .attr("r", self.circle.radius)
+        .style("fill", "red")
+        .attr("stroke", "red")
+        .attr("stroke-width", 3)
+        .attr("fill-opacity", 0.4);
+      this.svgGroup
+        .selectAll("circle")
+        .data(circleCoords)
+        .attr("r", self.circle.radius);
+    },
     // initLayers() {},
-    zoomUpdated(zoom: number) {
+    zoomUpdated(zoom: { start: number; end: number }) {
       this.zoom = zoom;
     },
     centerUpdated(center: [number, number]) {
--- webapp/frontend/src/main.ts
+++ webapp/frontend/src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./components/App.vue";
 import router from "./router";
 import store from "./store";
