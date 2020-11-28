efba373422333c3ce6060c1ebc62173e4e84cdfe
Bryce Williams <bwilliams2@gmail.com>
2020-11-27 20:34:49 -0600

    Fix app imports

--- webapp/backend/backend/apps/geoapi/urls.py
+++
@@ -1,7 +0,0 @@
-
-from django.urls import path, re_path
-from backend.geoapi.urls import urlpatterns as api_urls
-from backend.geoapi.views import index
-
-urlpatterns = [
-]
--- webapp/backend/backend/settings.py
+++ webapp/backend/backend/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -39,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.geoapi'
+    'backend.site_apps.geoapi'
 ]
 
 MIDDLEWARE = [
