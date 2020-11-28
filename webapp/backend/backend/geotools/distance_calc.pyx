46b4d9b96aa1f9d79c628aef7cfe77a2654e305a
Bryce Williams <bwilliams2@gmail.com>
2020-11-27 19:31:44 -0600

    Move to django rest hybrid app

--- webapp/backend/backend/geotools/distance_calc.pyx
+++ webapp/backend/backend/geotools/distance_calc.pyx
@@ -6,7 +6,7 @@ cdef extern from "/home/bryce/code/commute/tools/cShapeTools/include/PathFinder/
     void geoSearch(char * fname, double baseLong, double baseLat, int searchRadius, double * distances)
 
 cpdef get_distances(bytes fname, double baseLong, double baseLat, int searchRadius, int recordLength):
-    distances_array = np.ascontiguousarray(np.zeros((recordLength, dtype=np.double) - 1.)
+    distances_array = np.ascontiguousarray(np.zeros(recordLength, dtype=np.double) - 1.)
     cdef double[::1] distances = distances_array
     cdef char * cfname = fname
     geoSearch(cfname, baseLong, baseLat, searchRadius, &distances[0])
